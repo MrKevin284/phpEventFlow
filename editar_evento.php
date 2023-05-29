335e469e2b4073f7793e7630174fb859f7720c20
Kevin Souza <kevin.lucas284sz@gmail.com>
2023-05-29 16:54:22 -0300

    updt16

--- editar_evento.php
+++ editar_evento.php
@@ -1,13 +1,12 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Evento</title>
 </head>
 <body>
-    <div class="logo_principal">
+
+<div class="logo_principal">
         <img src="assets/imagens/logo_fundo_removido.png" alt="Logo EventFlow">
     </div>
 
@@ -17,137 +16,86 @@
     <a href="login.php">Logout</a>
 
     <h1>Editar Evento</h1>
+<?php
+// Incluir o arquivo de conexão com o banco de dados
+require_once 'conexao.php';
 
-    <?php
-    // Verificar se o usuário está logado e é um usuário empresarial
-    session_start();
-    if (!isset($_SESSION['idusuario'])) {
-        header("location: login.php");
-        exit();
-    }
+// Verificar se o ID do evento foi fornecido via GET
+if (isset($_GET['id'])) {
+    $idEvento = $_GET['id'];
 
-    $idusuario = $_SESSION['idusuario'];
-    require_once "conexao.php";
-
-    $query_usuario = "SELECT tipo_user FROM usuario WHERE idusuario = $idusuario";
-    $resultado_usuario = mysqli_query($conexao, $query_usuario);
-    $row_usuario = mysqli_fetch_assoc($resultado_usuario);
-    $tipo_usuario = $row_usuario['tipo_user'];
-
-    if ($tipo_usuario != 2) {
-        echo "Acesso negado. Esta página é restrita para usuários empresariais.";
-        exit();
-    }
-
-    // Verificar se o ID do evento foi fornecido como parâmetro
-    if (!isset($_GET['id'])) {
-        echo "ID do evento não fornecido.";
-        exit();
-    }
-
-    $id_evento = $_GET['id'];
-
-    // Verificar se o formulário foi submetido
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verificar se o formulário foi enviado
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Obter os dados do formulário
-        $nome_evento = $_POST['nome_evento'];
+        $nomeEvento = $_POST['nome_evento'];
         $endereco = $_POST['endereco'];
         $descricao = $_POST['descricao'];
-        $data_evento = $_POST['data_evento'];
+        $dataEvento = $_POST['data_evento'];
         $horario = $_POST['horario'];
 
-        // Atualizar o evento no banco de dados
-        $atualizar_evento = "UPDATE eventos SET nome_evento = '$nome_evento', endereco = '$endereco', descricao = '$descricao', data_evento = '$data_evento', horario = '$horario' WHERE idevento = $id_evento";
+        // Atualizar os dados do evento no banco de dados
+        $queryAtualizarEvento = "UPDATE eventos SET nome_evento = '$nomeEvento', endereco = '$endereco', descricao = '$descricao', data_evento = '$dataEvento', horario = '$horario' WHERE idevento = $idEvento";
+        $resultadoAtualizarEvento = mysqli_query($conexao, $queryAtualizarEvento);
 
-        if (mysqli_query($conexao, $atualizar_evento)) {
-            echo "Evento atualizado com sucesso.";
-            echo "<br>";
-            echo '<a href="eventos_criados.php">Voltar para a lista de eventos criados</a>';
-        } else {
-            echo "Erro ao atualizar o evento: " . mysqli_error($conexao);
-        }
-
-        // Verificar se foram fornecidos ingressos
-        if (isset($_POST['ingressos'])) {
-            // Excluir os ingressos antigos do evento
-            $excluir_ingressos = "DELETE FROM ingresso WHERE idevento = $id_evento";
-            mysqli_query($conexao, $excluir_ingressos);
-
-            // Inserir os novos ingressos no banco de dados
-            foreach ($_POST['ingressos'] as $id_tipoingresso => $ingresso) {
-                $quantidade_ingressos = $ingresso['quantidade'];
-                $preco_ingresso = $ingresso['preco'];
-
-                $inserir_ingresso = "INSERT INTO ingresso (quantidade, valor, id_tipoingresso, idevento) VALUES ($quantidade_ingressos, $preco_ingresso, $id_tipoingresso, $id_evento)";
-                mysqli_query($conexao, $inserir_ingresso);
-            }
-        }
-
-        // Fechar a conexão com o banco de dados
-        mysqli_close($conexao);
-    } else {
-        // Consultar as informações do evento no banco de dados
-        $consulta_evento = "SELECT * FROM eventos WHERE idevento = $id_evento";
-        $resultado_evento = mysqli_query($conexao, $consulta_evento);
-        $dados_evento = mysqli_fetch_assoc($resultado_evento);
-
-        if (!$dados_evento) {
-            echo "Evento não encontrado.";
+        // Verificar se a atualização foi bem-sucedida
+        if ($resultadoAtualizarEvento) {
+            // Redirecionar para a página de eventos após a atualização bem-sucedida
+            header('Location: eventos.php');
             exit();
-        }
-
-        // Consultar os ingressos do evento no banco de dados
-        $consulta_ingressos = "SELECT ti.id_tipoingresso, ti.descricao AS nome_tipoingresso, i.quantidade, i.valor FROM tipo_ingresso ti LEFT JOIN ingresso i ON ti.id_tipoingresso = i.id_tipoingresso WHERE i.idevento = $id_evento";
-        $resultado_ingressos = mysqli_query($conexao, $consulta_ingressos);
-        $ingressos = array();
-
-        while ($ingresso = mysqli_fetch_assoc($resultado_ingressos)) {
-            $ingressos[$ingresso['id_tipoingresso']] = $ingresso;
+        } else {
+            // Exibir mensagem de erro caso a atualização tenha falhado
+            echo 'Erro ao atualizar o evento.';
         }
     }
-    ?>
 
-    <form method="POST" action="">
+    // Obter os dados do evento do banco de dados
+    $queryObterEvento = "SELECT * FROM eventos WHERE idevento = $idEvento";
+    $resultadoObterEvento = mysqli_query($conexao, $queryObterEvento);
+
+    // Verificar se o evento foi encontrado
+    if ($resultadoObterEvento && mysqli_num_rows($resultadoObterEvento) > 0) {
+        $evento = mysqli_fetch_assoc($resultadoObterEvento);
+    } else {
+        // Redirecionar para a página de eventos se o evento não for encontrado
+        header('Location: eventos.php');
+        exit();
+    }
+} else {
+    // Redirecionar para a página de eventos se o ID do evento não for fornecido
+    header('Location: eventos.php');
+    exit();
+}
+?>
+
+
+
+    <form method="POST">
         <label for="nome_evento">Nome do Evento:</label>
-        <input type="text" name="nome_evento" id="nome_evento" value="<?php echo $dados_evento['nome_evento']; ?>">
+        <input type="text" id="nome_evento" name="nome_evento" value="<?php echo $evento['nome_evento']; ?>"><br><br>
 
         <label for="endereco">Endereço:</label>
-        <input type="text" name="endereco" id="endereco" value="<?php echo $dados_evento['endereco']; ?>">
+        <input type="text" id="endereco" name="endereco" value="<?php echo $evento['endereco']; ?>"><br><br>
 
-        <label for="descricao">Descrição:</label>
-        <textarea name="descricao" id="descricao"><?php echo $dados_evento['descricao']; ?></textarea>
+        <label for="descricao">Descrição:</label><br>
+        <textarea id="descricao" name="descricao"><?php echo $evento['descricao']; ?></textarea><br><br>
 
         <label for="data_evento">Data do Evento:</label>
-        <input type="date" name="data_evento" id="data_evento" value="<?php echo $dados_evento['data_evento']; ?>">
+        <input type="date" id="data_evento" name="data_evento" value="<?php echo $evento['data_evento']; ?>"><br><br>
 
         <label for="horario">Horário:</label>
-        <input type="time" name="horario" id="horario" value="<?php echo $dados_evento['horario']; ?>">
+        <input type="time" id="horario" name="horario" value="<?php echo $evento['horario']; ?>"><br><br>
 
-        <h2>Ingressos:</h2>
+        <input type="submit" value="Atualizar">
+    </form>
 
-        <?php
-        // Consultar os tipos de ingresso no banco de dados
-        $consulta_tipo_ingresso = "SELECT * FROM tipo_ingresso";
-        $resultado_tipo_ingresso = mysqli_query($conexao, $consulta_tipo_ingresso);
+    <br>
 
-        while ($tipo_ingresso = mysqli_fetch_assoc($resultado_tipo_ingresso)) {
-            $id_tipoingresso = $tipo_ingresso['id_tipoingresso'];
-            $nome_tipoingresso = $tipo_ingresso['descricao'];
-            $quantidade = isset($ingressos[$id_tipoingresso]['quantidade']) ? $ingressos[$id_tipoingresso]['quantidade'] : '';
-            $valor = isset($ingressos[$id_tipoingresso]['valor']) ? $ingressos[$id_tipoingresso]['valor'] : '';
-
-            echo '<div>';
-            echo '<label for="quantidade_' . $id_tipoingresso . '">Quantidade de ' . $nome_tipoingresso . ':</label>';
-            echo '<input type="number" name="ingressos[' . $id_tipoingresso . '][quantidade]" id="quantidade_' . $id_tipoingresso . '" value="' . $quantidade . '">';
-
-            echo '<label for="valor_' . $id_tipoingresso . '">Valor de ' . $nome_tipoingresso . ':</label>';
-            echo '<input type="number" step="0.01" name="ingressos[' . $id_tipoingresso . '][valor]" id="valor_' . $id_tipoingresso . '" value="' . $valor . '">';
-
-            echo '</div>';
-        }
-        ?>
-
-        <input type="submit" value="Atualizar Evento">
+    <form method="POST">
+        <input type="submit" value="Excluir" onclick="return confirm('Tem certeza de que deseja excluir este evento?');">
     </form>
 </body>
 </html>
+
+
+
+
