73fa96592a7aefab487d2225119d5fd0ad8ff725
Kevin Souza <kevin.lucas284sz@gmail.com>
2023-05-30 09:38:15 -0300

    update16

--- info_evento.php
+++ info_evento.php
@@ -16,6 +16,15 @@
         // Incluir o arquivo de conexão com o banco de dados
         require_once "conexao.php";
 
+        // Função para verificar se o usuário é o criador do evento
+        function verificarCriadorEvento($id_evento, $idusuario, $conexao) {
+            // Consultar o banco de dados para verificar se o usuário é o criador do evento
+            $query = "SELECT idevento FROM eventos WHERE idevento = $id_evento AND idusuario = $idusuario";
+            $resultado = mysqli_query($conexao, $query);
+
+            return (mysqli_num_rows($resultado) > 0);
+        }
+
         // Verificar se o usuário está logado
         session_start();
         if (!isset($_SESSION['idusuario'])) {
@@ -89,7 +98,7 @@
                         echo '<p>Nenhum ingresso disponível para este evento.</p>';
                     }
 
-                    if ($tipo_usuario == 2) { // Cadastro Empresarial
+                    if ($tipo_usuario == 2 && verificarCriadorEvento($id_evento, $idusuario, $conexao)) { // Cadastro Empresarial
                         echo '<a href="editar_evento.php?id=' . $id_evento . '">Editar Evento</a>';
                     }
 
